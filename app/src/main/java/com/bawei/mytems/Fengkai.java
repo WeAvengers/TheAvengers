5eb26af2ad5763587ab5555ef89ac619042af2be
fengkai0516 <7348713887@qq.com>
2017-09-29 08:52:55 +0800

    冯凯第一次commit

--- app/src/main/java/com/bawei/mytems/Fengkai.java
+++ app/src/main/java/com/bawei/mytems/Fengkai.java
@@ -2,6 +2,7 @@ package com.bawei.mytems;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Fengkai extends AppCompatActivity {
 
@@ -9,5 +10,18 @@ public class Fengkai extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fengkai);
+
+        //第一次吐司
+        Toast.makeText(this,"你好世界",Toast.LENGTH_LONG).show();
+
+
+
+
+
+
+
+
+
+
     }
 }
